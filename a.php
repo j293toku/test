400301440a5ce230b48413f792ff926674e08ad4
j293toku <j520293x@mails.cc.ehime-u.ac.jp>
2024-06-20 01:49:27 +0900

    a.php

--- a.php
+++ a.php
@@ -1,6 +1,13 @@
 #!/usr/bin/php
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
-    // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+    // trim() 関数を使って行末の改行を削除
+    $line = trim($line);
+    
+    // strpos() 関数を使って、文字列 "abc" が含まれるかをチェック
+    if (strpos($line, 'abc') !== false) {
+      // 文字列 "abc" が含まれる場合、その行を表示
+      echo "$line\n";
+    }
   } 
 ?>
