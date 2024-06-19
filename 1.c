17665a903e32cde58375d61896ab261311fab3b9
j293toku <j520293x@mails.cc.ehime-u.ac.jp>
2024-06-20 00:42:57 +0900

    1.c

--- 1.c
+++ 1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void)  
+{  
   int n;
-
+  
   printf("n = ");
   scanf("%d", &n);
 
-  if( (n & 1) == 0 ){  // 括弧で囲むことにより、ビット演算の結果を比較
-    printf("%d は偶数です\n", n);
-  }else{
+  if( (n & 1) == 0 ){  //nの最下位ビットを調べ、1であればその数は奇数、0であればその数は偶数
+  printf("%d は偶数です\n", n);}  
+  else{
     printf("%d は奇数です\n", n);
   }
   return 0;
