6fe396024baf3f3736deb7a9b1cc85aee40ba61d
j293toku <j520293x@mails.cc.ehime-u.ac.jp>
2024-06-19 11:50:58 +0900

    1.c

--- 1.c
+++ 1.c
@@ -7,7 +7,7 @@ int main(void)
   printf("n = ");
   scanf("%d", &n);
 
-  if( n & 1 == 0 ){
+  if( (n & 1) == 0 ){  // 括弧で囲むことにより、ビット演算の結果を比較
     printf("%d は偶数です\n", n);
   }else{
     printf("%d は奇数です\n", n);
