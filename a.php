d98bdf738caaa2ba0e55babc4bc377029c8216a9
j293toku <j520293x@mails.cc.ehime-u.ac.jp>
2024-06-25 17:06:45 +0900

    a.phpをstrpos() 関数を使って、文字列 "xy" が含まれる単語を表示できるようにした

--- a.php
+++ a.php
@@ -1,9 +1,6 @@
 #!/usr/bin/php
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
-    // trim() 関数を使って行末の改行を削除
-    $line = trim($line);
-    
     // strpos() 関数を使って、文字列 "xy" が含まれるかをチェック
     if (strpos($line, 'xy') !== false) {
       // 文字列 "xy" が含まれる場合、その行を表示
