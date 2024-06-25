40e375184318bef2da7dfe9cb007b0e121cd0588
j293toku <j520293x@mails.cc.ehime-u.ac.jp>
2024-06-25 20:24:28 +0900

    // 修正: iの初期化を追加

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないと�
 
 int main(void)
 {
-  int i;
+  int i = 0; // iの初期化を追加
   double *x = (double *)malloc(sizeof(double) * 100);
 
   while( i <= 100 ){
