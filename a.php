914a5b9b9e68a42bf794ff1befba864a8a92f0df
j293toku <j520293x@mails.cc.ehime-u.ac.jp>
2024-06-25 13:26:03 +0900

    修正完了 a.php

--- a.php
+++ a.php
@@ -4,9 +4,9 @@
     // trim() 関数を使って行末の改行を削除
     $line = trim($line);
     
-    // strpos() 関数を使って、文字列 "abc" が含まれるかをチェック
-    if (strpos($line, 'abc') !== false) {
-      // 文字列 "abc" が含まれる場合、その行を表示
+    // strpos() 関数を使って、文字列 "xy" が含まれるかをチェック
+    if (strpos($line, 'xy') !== false) {
+      // 文字列 "xy" が含まれる場合、その行を表示
       echo "$line\n";
     }
   } 
